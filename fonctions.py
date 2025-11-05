a6d9aa73fbbfaf0d32d5ac278efa326683339e4d
Draiphis <val08350@gmail.com>
2025-11-05 12:03:13 +0100

    added(feat):added delete champ function

--- fonctions.py
+++ fonctions.py
@@ -1,6 +1,8 @@
 import csv
   
 
+
+
 def ajouter_un_champion():
     nom_existant=set()
     with open("champions.csv", 'r', encoding='utf-8', newline='') as f:
@@ -32,6 +34,8 @@ def ajouter_un_champion():
             continue
         else:
             Role=Roles[numero_role]
+
+
         print(Classes)
         numero_classe=input(f"Indiquez le numéro correspondant à la classe de {Nom} (appuyez sur q pour quitter) : ")
         if numero_classe.strip().lower()=="q":
@@ -50,18 +54,32 @@ def ajouter_un_champion():
             continue
         else:
             Degat=Degats[numero_degat]
+        
+        
+        played=input(f"Avez-vous déjà joué {Nom} (Oui ou non //  appuyez sur q pour quitter) : ")
+        if played.strip().lower()=="q":
+            break
+        if played.strip().lower()!= ("oui" or "non"):
+            print("choix invalide")
+            continue
+        else:
+            play=played
         with open("champions.csv", 'a', encoding='utf-8', newline='') as f:
             writer = csv.writer(f)
-            writer.writerow([Nom.strip().capitalize(), Classe, Role, Degat])
+            writer.writerow([Nom.strip().capitalize(), Classe, Role, Degat,play.strip().capitalize()])
         break
     
+
+
         
 def afficher_champions():
     with open("champions.csv", 'r', encoding='utf-8', newline='') as f:
         reader=csv.DictReader(f)
         for ligne in reader:
-            affichage = f"Nom => {ligne['Nom']} | Role => {ligne['Role']} | Classe => {ligne['Classe']} | Type_de_Dégats => {ligne['Type_de_Dégats']}"
+            affichage = f"Nom => {ligne['Nom']} | Role => {ligne['Role']} | Classe => {ligne['Classe']} | Type_de_Dégats => {ligne['Type_de_Dégats']} | {ligne["Déjà_Joué"]}"
             print(f"{affichage}\n")
+
+
 
 def rechercher_un_champion():
     with open("champions.csv", "r", encoding="utf-8") as f:
@@ -73,10 +91,42 @@ def rechercher_un_champion():
                 affichage = f"Nom => {champion['Nom']} | Role => {champion['Role']} | Classe => {champion['Classe']} | Type_de_Dégats => {champion['Type_de_Dégats']}"
                 print(f"\n{affichage}\n")
 
+def supprimer_un_champion():
+    
+    with open("champions.csv", "r", encoding="utf-8") as f:
+        reader = list(csv.DictReader(f))
+        fieldnames = reader[0].keys()
+
+    
+    suppression = input("Entrez le nom du champion : ").strip()
+
+    
+    count = -1
+    for i, ligne in enumerate(reader):
+        if ligne["Nom"].lower() == suppression.lower():
+            count = i
+            break
+
+    if count == -1:
+        print(f"Le champion '{suppression}' n'a pas été trouvé.")
+        return
+
+    
+    del reader[count]
+
+    
+    with open("champions.csv", "w", encoding="utf-8", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(reader)
+
+    print(f"Le champion '{suppression}' a été supprimé avec succès.")
+
+
 
 def menu():
     while True :
-        menus={"1":"Afficher les champions","2":"Ajouter des champions","3":"Rechercher des champions","q":"Quitter"}
+        menus={"1":"Afficher les champions","2":"Ajouter des champions","3":"Rechercher des champions","4":"Supprimer un champion" ,"q":"Quitter"}
         for cle, valeur in menus.items():
             print(f"{cle} : {valeur}")
         choix=input("Que voulez vous faire ? (Entrez le numéro correspondant ou q) : ")
@@ -100,6 +150,13 @@ def menu():
         if choix.strip().lower()== "3":
             rechercher_un_champion()
             continue
+        if choix.strip().lower()== "4":
+            supprimer_un_champion()
+            continue
+
+
+
+
 
 menu()
 
